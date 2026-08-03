d69ff3411947f2eef3b49647b3a2e86cc4c5fc13
JanaElzarka <jojoelzarka@gmail.com>
2026-08-03 18:39:25 +0300

    logout-edited

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
+int main ()
+{ 
+   /*int n;
+cout << "enter the number of lines: ";
+cin >> n;
+cout << endl;
+int arr[n][3];
+for (int i=0;i<n;i++)
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    for(int j=0;j<3;j++)
+    {
+        cin >> arr[i][j];
+    }
 }
+int sum=0;
+for (int i=0;i<n;i++)
+{
+    for(int j=0;j<3;j++)
+    {
+      sum+= arr[i][j];
+    }
+}
+if(sum==0)
+    cout << "yes.";
+else cout << "No."; */
+int n;
+cout << "enter the number of layers: ";
+cin >> n;
+for(int i=1;i<=n;i++)
+{
+    if (i%2 != 0)
+        cout << " I hate";
+    else cout << " I love";
+    if(i!=n) cout << " that";
+}
+cout << " it";
+return 0;
+}
+
+
